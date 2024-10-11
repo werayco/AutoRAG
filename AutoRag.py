30508fe230ae1eadfd065d7541588871500c0e47
Ray <142559582+werayco@users.noreply.github.com>
2024-10-11 04:42:32 -0700

    final-update-rag

--- AutoRag.py
+++ AutoRag.py
@@ -1,18 +1,16 @@
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_ollama.llms import OllamaLLM
 from langchain_ollama import ChatOllama
 from langchain.vectorstores import FAISS
-from typings import str
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.output_parsers import (
     StructuredOutputParser,
     ResponseSchema,
     CommaSeparatedListOutputParser,
 )
-
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class AutoRAG:
@@ -24,38 +22,6 @@ class AutoRAG:
         self.temp = temp
         self.llm = ChatOllama(model=self.model, temperature=self.temp)
 
-    def RAGProcess(self, input) :
-        
-        pdf = PyPDFLoader(self.path_to_doc)
-        documents = pdf.load()
-
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
-        )
-        text = splitter.split_documents(documents=documents)
-
-        embedder = HuggingFaceEmbeddings()
-        faiss_db = FAISS.from_documents(text, embedder)
-        retriever = faiss_db.as_retriever(
-            search_type="mmr", search_kwargs={"k": 6, "lambda_mult": 0.25}
-        )
-
-        temp = """
-        Answer the follow questions based on this document:
-        <context>
-        {document}
-        <context/>
-        {input}
-        """
-        ptemp = PromptTemplate.from_template(template=temp)
-        doc_chain = create_stuff_documents_chain(self.llm, ptemp)
-
-        retriever_qa = create_retrieval_chain(
-            retriever=retriever, combine_docs_chain=doc_chain
-        )
-        query = retriever_qa.invoke({"input": input, "document": documents})
-
-        return {"response":query}
 
     def parser(self, country, gender, language, term):
         schemas = [
@@ -111,6 +77,38 @@ class AutoRAG:
         comma_response_llm = comma_parser.parse(self.llm(messg))
         return {"StructuredOutputJson":structured_parsed_op, "ListOutput":comma_response_llm}
 
+    def RAGProcess(self, input) :
+        
+        pdf = UnstructuredPDFLoader(self.path_to_doc)
+        documents = pdf.load()
+
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
+        )
+        text = splitter.split_documents(documents=documents)
+
+        embedder = HuggingFaceEmbeddings()
+        faiss_db = FAISS.from_documents(text, embedder)
+        retriever = faiss_db.as_retriever(
+            search_type="similarity_score_threshold", search_kwargs={"k": 6, "lambda_mult": 0.25}
+        )
+
+        query_temp = PromptTemplate.from_template("""You are an AI assistant. Your task is to generate 5 different version
+                                                  of the user's query and make similary serach with the vector database provided {question}""")
+        
+        retriever_ = MultiQueryRetriever.from_llm(retriever=retriever,llm=self.llm,prompt=query_temp)
+
+        template = """ Answer the question bases on the following context:
+        {context}
+        question: {question}
+        """
+        prompt = ChatPromptTemplate.from_template(template)
+    
+        chain = ({"context":retriever_, "question": RunnablePassthrough()} | prompt | self.llm | StrOutputParser() )
+
+        response = chain.invoke(input)
+
+        return {"response":response}
 
 if __name__ == "__main__":
     autorag = AutoRAG(model="nemotron-mini",chunk_size=100, chunk_overlap=10,path_to_doc="your_path_file_path_goes_here.pdf",temp=0.7)
